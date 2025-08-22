a40fc190122e7c8ec4386c17d70b7844c3934432
naumpopov18-art <naum.popov.18@gmail.com>
2025-08-22 19:23:58 +0500

    Update utils.py

--- stroycent/utils.py
+++ stroycent/utils.py
@@ -1,7 +1,18 @@
+import logging
+
 log_file = None
 
 def debug_log(msg):
-    print(f"[DEBUG] {msg}")
+    # Используем стандартное логирование вместо print
+    logging.debug(msg)
+    
+    # Сохраняем вашу функциональность с файлом
     if log_file:
         log_file.write(f"[DEBUG] {msg}\n")
         log_file.flush()
+
+# Настройка логирования
+logging.basicConfig(level=logging.DEBUG)
+
+# Использование
+debug_log("Загрузка этажа 0")
